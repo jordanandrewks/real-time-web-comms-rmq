3bb42b8513d55b71a52ecb6f69624478454c8894
Jordan S. K <26313566+jordanandrewks@users.noreply.github.com>
2024-04-22 22:51:32 +0100

    spring boot config - init

---
+++ real-time-web-comms-messaging-rabbitmq/src/main/kotlin/com/example/realtimewebcommsmessagingrabbitmq/RealTimeWebCommsMessagingRabbitmqApplication.kt
@@ -0,0 +1,11 @@
+package com.example.realtimewebcommsmessagingrabbitmq
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RealTimeWebCommsMessagingRabbitmqApplication
+
+fun main(args: Array<String>) {
+	runApplication<RealTimeWebCommsMessagingRabbitmqApplication>(*args)
+}
---
+++ real-time-web-comms-messaging-rabbitmq/src/test/kotlin/com/example/realtimewebcommsmessagingrabbitmq/RealTimeWebCommsMessagingRabbitmqApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.realtimewebcommsmessagingrabbitmq
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RealTimeWebCommsMessagingRabbitmqApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
